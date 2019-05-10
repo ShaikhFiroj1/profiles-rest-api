6b6b89a3ea84199f970288a48ebb70b3c00c34f3
Firoj Shaikh <firojshaikh980.com>
2019-05-10 08:13:05 +0530

    added improved permissions to user profile feed item

--- src/profiles_project/profiles_api/permissions.py
+++ src/profiles_project/profiles_api/permissions.py
@@ -11,3 +11,15 @@ class UpdateOwnProfile(permissions.BasePermission):
             return True
 
         return obj.id == request.user.id
+
+
+class PostOwnStatus(permissions.BasePermission):
+    """ Allow user to update theri own statuses"""
+
+    def has_object_permission(self, request, view, obj):
+        """ Check user is trying to edit thier own status"""
+
+        if request.method in permissions.SAFE_METHODS:
+            return True
+
+        return obj.user_profile.id == request.user.id
